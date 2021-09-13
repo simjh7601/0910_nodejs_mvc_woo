1ec56e66ae9e19eed0ddf0c22d455a799fa71b76
simjh <simjh7601@naver.com>
2021-09-13 17:54:58 +0900

    0913완료 0914다시 진행하기

---
+++ app/src/models/User.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const UserStorage = require("./UserStorage");
+
+
+
+class User{
+    constructor(body){
+        this.body = body;
+    }
+    login() {
+        const body = this.body;
+        const {id, psword}  = UserStorage.getUserInfo(this.body.id);
+
+        if(id){
+            if(id === body.id && psword === body.psword ){
+                return {success : true };
+            }
+            return {success : false, msg:"비밀번호가 틀렸어..."};
+        }
+        return {success : false, msg:"존재하지 않는 아이디"};
+    }
+
+}
+
+module.exports = User;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,6 +2,7 @@
 
 
 
+const User = require("../../models/User");
 const { getUsers } = require("../../models/UserStorage");
 const UserStorage = require("../../models/UserStorage")
 // 좀더 직관적으로 알려주기 위한 변수명 output 로 객체생성
@@ -21,25 +22,29 @@ const output = {
 const porcess = {
     // req 는 프론트단에서 보내준 정보를 담아두는 객체
     login : (req, res) =>{
-        const id = req.body.id,
-        psword = req.body.psword;
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+       // return res.json(response);
+    //     const id = req.body.id,
+    //     psword = req.body.psword;
 
-     //   const UserStorage = new UserStorage();
-        const users = UserStorage.getUsers("id","psword");
+    //  //   const UserStorage = new UserStorage();
+    //     const users = UserStorage.getUsers("id","psword");
 
-         const response = {};
-        if(users.id.includes(id) ) {
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] == psword){
-                response.success = true;
-                return res.json({
-                });
-            }
-        }
+    //      const response = {};
+    //     if(users.id.includes(id) ) {
+    //         const idx = users.id.indexOf(id);
+    //         if(users.psword[idx] == psword){
+    //             response.success = true;
+    //             return res.json({
+    //             });
+    //         }
+    //     }
 
-        response.success = false;
-        response.msg = "로그인 실패"
-        return res.json(response);
+    //     response.success = false;
+    //     response.msg = "로그인 실패"
+    //     return res.json(response);
     } ,
 };
 
@@ -49,3 +54,5 @@ module.exports = {
     porcess,
     
 };
+
+
